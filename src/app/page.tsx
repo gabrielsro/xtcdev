c5887c1ddeebc1d3b63e33e37b3860725a8f7d26
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-30 13:28:56 -0400

    Home page aesthetic modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import PulseLoader from "@/ui/loaders/pulseLoader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-60">
       <div className="text-teal-300 text-5xl font-semibold">XTCDEV</div>
       <div className="flex items-center gap-2">
         <PulseLoader />
-        <p>Website under construction</p>
+        <p className="whitespace-nowrap">Website under construction</p>
         <PulseLoader />
       </div>
     </main>
