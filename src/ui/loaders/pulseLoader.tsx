a60bf07f0db5711f7dc62cd0caea2ae8d522d080
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-30 13:17:09 -0400

    Basic homepage

---
+++ src/ui/loaders/pulseLoader.tsx
@@ -0,0 +1,27 @@
+export default function PulseLoader() {
+  return (
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="rgb(94, 234, 212)"
+    >
+      <path
+        className="spinner_Uvk8"
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+        transform="translate(12, 12) scale(0)"
+      />
+      <path
+        className="spinner_Uvk8 spinner_ypeD"
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+        transform="translate(12, 12) scale(0)"
+      />
+      <path
+        className="spinner_Uvk8 spinner_y0Rj"
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+        transform="translate(12, 12) scale(0)"
+      />
+    </svg>
+  );
+}
