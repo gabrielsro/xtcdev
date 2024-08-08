baa776cd5f6d396234be45b9da4e2de043dc4fa7
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-08-08 16:09:13 -0400

    Basic globe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import TopNav from "./_components/topnav";
 import BottomNav from "./_components/bottomnav";
 import { StarsBackground } from "@/ui/effects/stars-background";
 import { ShootingStars } from "@/ui/effects/shooting-stars";
+import { World } from "@/ui/effects/globe";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +33,7 @@ export default function RootLayout({
             />
             <main className="relative">{children}</main>
           </div>
-          <BottomNav />
+          {/* <BottomNav /> */}
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,20 @@
-import PulseLoader from "@/ui/loaders/pulseLoader";
+import World from "@/ui/effects/world";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between pt-24 pb-60">
+    <div className="flex flex-col items-center justify-between pt-16 md:pt-20 pb-60">
       <div className="text-teal-300 text-5xl font-semibold">XTCDEV</div>
       <div className="flex items-center gap-2">
-        {/* <PulseLoader /> */}
         <p className="whitespace-nowrap">Website under construction</p>
-        {/* <PulseLoader /> */}
       </div>
-      <div className="px-20 py-20">
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod a vulputate
-        vel nullam placerat curabitur nunc scelerisque sagittis. Curabitur
-        porttitor litora, himenaeos eros venenatis mi. Torquent gravida
-        tristique ante accumsan pellentesque habitasse in. Curae mollis blandit
-        sapien tortor commodo nunc varius habitasse. Massa vehicula sagittis
-        inceptos montes nascetur sollicitudin. Curabitur elementum pellentesque
-        sem viverra fermentum habitant viverra sapien. Placerat fusce cursus
-        eleifend nulla tempus, magnis ultricies. Maximus morbi quis sapien;
-        hendrerit scelerisque eleifend phasellus! Lobortis viverra primis
-        nascetur molestie quisque semper. Ornare neque sed sed tempus, donec
-        nibh dignissim elit. Dictum adipiscing augue ligula odio torquent tellus
-        pharetra nisi. At tellus accumsan platea eu aptent fusce mus metus.
-        Augue cursus velit laoreet amet curae cursus fermentum massa. Finibus
-        pretium sem mauris dolor bibendum. Nostra aptent lobortis fringilla
-        gravida turpis morbi fringilla, auctor fringilla. Lobortis scelerisque
-        velit porttitor euismod bibendum pulvinar fermentum nec litora. At
-        posuere hac curae sit mi iaculis ut. Sed pharetra nulla libero mauris eu
-        conubia penatibus. Dapibus convallis donec gravida sociosqu sodales non.
-        Tortor integer erat eget curae hendrerit scelerisque. Dapibus augue
-        sociosqu imperdiet aptent vehicula adipiscing sodales. Velit vel
-        dignissim libero morbi tellus mollis curae? Lorem finibus lacus
-        adipiscing nullam id condimentum aliquam sit. Eleifend hac ad vivamus
-        rhoncus conubia velit varius felis. Non nam porta nullam leo porta.
-        Dictumst ipsum pellentesque habitasse; suspendisse accumsan efficitur.
-        Nascetur tellus augue aliquet; velit lectus duis integer. Auctor
-        habitant himenaeos semper aptent odio hendrerit integer porta hac? Eget
-        viverra parturient risus platea quis mattis vestibulum in. Pellentesque
-        iaculis arcu risus venenatis orci lectus. Bibendum primis volutpat
-        imperdiet sed faucibus fames amet nunc. Libero maximus purus velit diam
-        ultrices varius taciti dui bibendum. Lacus nostra accumsan vitae nunc
-        etiam vestibulum. Quis eros himenaeos id natoque; donec commodo. Ornare
-        porta aliquet gravida netus a facilisis facilisis adipiscing. In platea
-        curabitur condimentum duis et platea elit mus. Elit montes posuere
-        primis erat erat eu felis nec. Sed duis nascetur fringilla molestie
-        aptent enim rhoncus ridiculus posuere. Nostra curae erat habitasse
-        semper sit. Finibus sodales nulla senectus, porttitor tempor porta. At
-        consequat at posuere potenti nascetur at; per eu? Parturient iaculis
-        aptent neque mi dictum mi dignissim habitant.
+
+      <div className=" lg:flex lg:items-center pt-14 md:pt-0 relative">
+        <World />
+        <div className="absolute top-40 right-0">
+          <p>+1 4632398161</p>
+          <p>xtcdev@gmail.com</p>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
