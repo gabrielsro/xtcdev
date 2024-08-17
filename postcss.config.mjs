05c8b89f4de5163a99ffb32749304490d653add8
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-08-17 18:13:28 -0400

    Enabled next rewrites as reverse proxy for analytics

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  async rewrites(){
+    return [{source:"/ingest/static/:path*",destination:"https://us-assets.i.posthog.com"},{source:"/ingest/:path*",destination:"https://us.i.posthog.com/path*"},{source:"ingest/decide",destination:"https://us.i.posthog.com/decide"}];
+  },
+  skipTrailingSlashRedirect:true,
   plugins: {
     tailwindcss: {},
   },
